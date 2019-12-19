b4a48226bc0a70813d1b2fe4cb8d1257e12d740a
Mehul <318097@gmail.com>
2019-12-19 16:36:58 +0530

    Refactor

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
+const BASE_URL =
+  process.env.REACT_APP_NODE_ENV === "development"
+    ? "http://localhost:7000"
+    : "https://bubblegum-server.herokuapp.com";
+
 const config = {
-  SERVER_URL: process.env.REACT_APP_NODE_ENV === 'development' ?
-    'http://localhost:7000/api' :
-    'https://bubblegum-server.herokuapp.com/api',
+  SERVER_URL: `${BASE_URL}/api`,
+  SOCKET_URL: `${BASE_URL}/chat`
 };
 
 module.exports = config;
